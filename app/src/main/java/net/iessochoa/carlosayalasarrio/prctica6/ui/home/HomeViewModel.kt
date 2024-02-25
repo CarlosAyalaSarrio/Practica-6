5bcd10839e54ef913673eef8f100727a22ec82aa
Carlos <CarlosAyalaSarrio18@gmail.com>
2024-02-25 04:19:49 +0100

    ViewModel

--- app/src/main/java/net/iessochoa/carlosayalasarrio/prctica6/ui/home/HomeViewModel.kt
+++ app/src/main/java/net/iessochoa/carlosayalasarrio/prctica6/ui/home/HomeViewModel.kt
@@ -1,13 +1,20 @@
 package net.iessochoa.carlosayalasarrio.prctica6.ui.home
 
+import android.app.Application
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.iessochoa.carlosayalasarrio.prctica6.network.NetworkService
 
-class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+object Repository {
+    //necesitaremos el context en las capas inferiores
+    private lateinit var application: Application
+    //iniciamos la instancia
+    operator fun invoke(context: Context) {
+        this.application = context.applicationContext as Application
+        NetworkService(application)
     }
-    val text: LiveData<String> = _text
+    suspend fun getNextPersonajes()=NetworkService.getNextPersonajes()
+    fun getLiveDataListaPersonajes ()=NetworkService.getLiveDataListaPersonajes()
 }
