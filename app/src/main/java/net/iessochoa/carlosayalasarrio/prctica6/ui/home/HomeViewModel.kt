c8a5e1b2f96a71a38082227a7118f363bee85c72
Carlos <CarlosAyalaSarrio18@gmail.com>
2024-02-25 04:34:36 +0100

    network

--- app/src/main/java/net/iessochoa/carlosayalasarrio/prctica6/ui/home/HomeViewModel.kt
+++ app/src/main/java/net/iessochoa/carlosayalasarrio/prctica6/ui/home/HomeViewModel.kt
@@ -1,20 +1,26 @@
 package net.iessochoa.carlosayalasarrio.prctica6.ui.home
 
 import android.app.Application
-import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import net.iessochoa.carlosayalasarrio.prctica6.network.NetworkService
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import net.iessochoa.carlosayalasarrio.prctica6.model.Personaje
+import net.iessochoa.carlosayalasarrio.prctica6.repository.Repository
 
-object Repository {
-    //necesitaremos el context en las capas inferiores
-    private lateinit var application: Application
-    //iniciamos la instancia
-    operator fun invoke(context: Context) {
-        this.application = context.applicationContext as Application
-        NetworkService(application)
+class HomeViewModel (application: Application): AndroidViewModel(application) {
+    val personajesLiveData:LiveData<MutableList<Personaje>>
+    init {
+        //iniciamos el repositorio
+        Repository(application)
+        //observaremos los cambios en la lista de personajes
+        personajesLiveData= Repository.getLiveDataListaPersonajes()
+        //carga la primera página de personajes
+        getNextPersonajes()
     }
-    suspend fun getNextPersonajes()=NetworkService.getNextPersonajes()
-    fun getLiveDataListaPersonajes ()=NetworkService.getLiveDataListaPersonajes()
+    //lanza en segundo plano la carga de nuevos personajes
+    fun getNextPersonajes()=viewModelScope.launch(Dispatchers.IO) {
+        Repository.getNextPersonajes()
+    }
 }
